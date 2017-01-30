4ff94a09de2563619a203cadd33f816d69483dfa
sschoger <steveschoger@Steves-MacBook-Pro-2.local>
2017-01-30 08:49:32 -0500

    170130-patterns

--- js/app.js
+++ js/app.js
@@ -10,9 +10,11 @@ var vm = new Vue({
             fgColor: '#9C92AC',
             opacity: .4,
         },
-
-
         patterns: [{
+            name: 'Falling Triangles',
+            image: '<svg xmlns="http://www.w3.org/2000/svg" width="36" height="72" viewBox="0 0 36 72"><g fill-rule="evenodd"><g fill="#000"><path d="M2 6h12L8 18 2 6zm18 36h12l-6 12-6-12z"/></g></g></svg>',
+            download: './svg/falling-triangles.zip'
+        }, {
             name: 'Piano Man',
             image: '<svg xmlns="http://www.w3.org/2000/svg" width="70" height="46" viewBox="0 0 70 46"><g fill-rule="evenodd"><g fill="#000"><polygon points="68 44 62 44 62 46 56 46 56 44 52 44 52 46 46 46 46 44 40 44 40 46 38 46 38 44 32 44 32 46 26 46 26 44 22 44 22 46 16 46 16 44 12 44 12 46 6 46 6 44 0 44 0 42 8 42 8 28 6 28 6 0 12 0 12 28 10 28 10 42 18 42 18 28 16 28 16 0 22 0 22 28 20 28 20 42 28 42 28 28 26 28 26 0 32 0 32 28 30 28 30 42 38 42 38 0 40 0 40 42 48 42 48 28 46 28 46 0 52 0 52 28 50 28 50 42 58 42 58 28 56 28 56 0 62 0 62 28 60 28 60 42 68 42 68 0 70 0 70 46 68 46"/></g></g></svg>',
             download: './svg/piano-man.zip'
