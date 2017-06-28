f603dbfaef46d6d0fe51cf95ed651857f393761f
Steve Schoger <sm.schoger@gmail.com>
2017-06-28 11:13:16 -0400

    170628-pattern-fix

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ var vm = new Vue({
         },
         patterns: [{
             name: 'Overcast',
-            image: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 80" width="80" height="80"><g fill="#000"><path d="M0 0h80v80H0V0zm20 20v40h40V20H20zm20 35a15 15 0 1 1 0-30 15 15 0 0 1 0 30z"></path><path d="M15 15h50l-5 5H20v40l-5 5V15zm0 50h50V15L80 0v80H0l15-15zm32.07-32.07l3.54-3.54A15 15 0 0 1 29.4 50.6l3.53-3.53a10 10 0 1 0 14.14-14.14zM32.93 47.07a10 10 0 1 1 14.14-14.14L32.93 47.07z"></path></g></svg>',
+            image: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 80" width="80" height="80"><g fill="#000"><path d="M0 0h80v80H0V0zm20 20v40h40V20H20zm20 35a15 15 0 1 1 0-30 15 15 0 0 1 0 30z" opacity=".5"></path><path d="M15 15h50l-5 5H20v40l-5 5V15zm0 50h50V15L80 0v80H0l15-15zm32.07-32.07l3.54-3.54A15 15 0 0 1 29.4 50.6l3.53-3.53a10 10 0 1 0 14.14-14.14zM32.93 47.07a10 10 0 1 1 14.14-14.14L32.93 47.07z"></path></g></svg>',
             download: './svg/overcast.zip'
         }, {
             name: 'Formal Invitation',
